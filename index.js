544c4b8e13f1db3527e896158cbe440878c7d13f
Hari Prasath <hariprasath.s@embarckle.com>
2023-04-07 18:02:44 +0530

    changed some code in style, Added button

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
+let data_;
 fetch("https://type.fit/api/quotes")
   .then(function(response) {
     return response.json();
   })
   .then(function(data) {
+    data_ = data
     displayquotes(data);
-  console.log(data)
   });
 const quote = document.querySelector('.quotes');
 const author = document.querySelector('.teller');
 
 function displayquotes(data){
-
   const keys = Object.values(data);
  	$quote = keys[Math.floor(Math.random() * keys.length)];
   quote.innerText = `"${$quote.text}"`;
@@ -18,4 +18,10 @@ function displayquotes(data){
   author.innerText = `- ${$quote.author}`
   }
 }
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click',()=>{
+    displayquotes(data_)
+})
 
